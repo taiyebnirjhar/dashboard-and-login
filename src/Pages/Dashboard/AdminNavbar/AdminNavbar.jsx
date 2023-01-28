0cfac15e8df578ca1b042b63bc20a4fd2772facc
taiyebnirjhar <nirjhar.212902003.green.ac.bd>
2023-01-28 19:55:58 +0600

    updating  dash sidebar

--- src/Pages/Dashboard/AdminNavbar/AdminNavbar.jsx
+++ src/Pages/Dashboard/AdminNavbar/AdminNavbar.jsx
@@ -1,21 +1,29 @@
 import React from "react";
-
+import presentation from "../../../assets/Icons/Presentation.svg";
 function AdminNavbar() {
   return (
     <>
       {/* Navbar */}
       <nav className="absolute top-0 left-0 w-full z-10  md:flex-row md:flex-nowrap md:justify-start flex items-center p-4 ">
-        <div className="w-full mx-autp items-center flex justify-between md:flex-nowrap flex-wrap md:px-10 px-4">
+        <div className="w-full mx-aut0 items-center flex justify-around md:flex-nowrap flex-wrap md:px-10 px-4">
           {/* Brand */}
-          <a
-            className=" text-[18px] font-[700]  hidden lg:inline-block leading-[25px]"
-            href="#"
-            onClick={(e) => e.preventDefault()}
-          >
+          <div className=" text-[18px] font-[700]  hidden lg:inline-block leading-[25px]">
             Dashboard
-          </a>
+          </div>
+          <div>
+            {/* Button --- start trading */}
+            <div className=" bg-[#0AB062] rounded-[6px] flex gap-3  px-6 py-3 ">
+              <div className="flex self-center">
+                <img src={presentation} alt="" srcset="" />
+              </div>
+              <div className="text-white  font-[600] leading-[19px] text-[14px] text-center">
+                Start Trading
+              </div>
+            </div>
+          </div>
+
           {/* Form */}
-          <form className="md:flex hidden flex-row flex-wrap items-center lg:ml-auto mr-3">
+          {/* <div className="md:flex hidden flex-row flex-wrap items-center mr-3">
             <div className="relative flex w-full flex-wrap items-stretch">
               <span className="z-10 h-full leading-snug font-normal absolute text-center  rounded text-base items-center justify-center w-8 pl-3 py-3">
                 <i className="fas fa-search"></i>
@@ -26,11 +34,11 @@ function AdminNavbar() {
                 className="border-0 px-3 py-3 placeholder-blueGray-300  relative   rounded text-sm shadow outline-none focus:outline-none focus:ring w-full pl-10"
               />
             </div>
-          </form>
+          </div> */}
           {/* User */}
-          <ul className="flex-col md:flex-row list-none items-center hidden md:flex">
-            {/* <UserDropdown /> */}
-          </ul>
+          {/* <ul className="flex-col md:flex-row list-none items-center hidden md:flex">
+            
+          </ul> */}
         </div>
       </nav>
       {/* End Navbar */}
--- src/Pages/Dashboard/Sidebar/Sidebar.jsx
+++ src/Pages/Dashboard/Sidebar/Sidebar.jsx
@@ -1,15 +1,65 @@
 import React, { useState } from "react";
+import Billing from "../../../assets/Icons/Billing.svg";
+import dashboard from "../../../assets/Icons/Dashboard.svg";
+import dollar from "../../../assets/Icons/Dollar.svg";
+import Question from "../../../assets/Icons/Question.svg";
+import news from "../../../assets/Icons/news.svg";
+import overview from "../../../assets/Icons/overview.svg";
+import reset from "../../../assets/Icons/reset.svg";
+import topup from "../../../assets/Icons/topup.svg";
+import utilities from "../../../assets/Icons/utilities.svg";
+
 import logo from "../../../assets/logo.svg";
 
 function Sidebar() {
+  /******** STATE ************/
   const [collapseShow, setCollapseShow] = useState("hidden");
+  /*************************/
+
+  /********  DRY  **********/
+  const [menus, setMenues] = useState([
+    {
+      name: "Dashboard",
+      icon: dashboard,
+    },
+    {
+      name: "Trading Overview",
+      icon: overview,
+    },
+    {
+      name: "Utilities",
+      icon: utilities,
+    },
+    {
+      name: "Withdraw",
+      icon: dollar,
+    },
+    {
+      name: "Top-Up & Reset",
+      icon: topup,
+    },
+
+    {
+      name: "Billing",
+      icon: Billing,
+    },
+    {
+      name: "News Calendar",
+      icon: news,
+    },
+    {
+      name: "Help",
+      icon: Question,
+    },
+  ]);
+  /************************/
 
   return (
     <>
       <nav className="md:left-0 md:block md:fixed md:top-0 md:bottom-0 md:overflow-y-auto md:flex-row md:flex-nowrap md:overflow-hidden shadow-xl flex flex-wrap items-center justify-between relative md:w-64 z-10 py-4 px-4  backgroundForth">
         <div className="md:flex-col md:items-stretch md:min-h-[100%] md:flex-nowrap px-6 flex flex-wrap items-center justify-between w-full mx-auto backgroundThird rounded-md text-white ">
           {/***********************/}
-          {/* Toggler */}
+          {/* Menu Toggler */}
           <button
             className="cursor-pointer text-black opacity-50 md:hidden px-3 py-1 text-xl leading-none bg-transparent rounded border border-solid border-transparent"
             type="button"
@@ -30,20 +80,20 @@ function Sidebar() {
               <div className="text-[26px] font-[600] leading-[31px] tracking-[0.1rem] text-[#0AB062] tommorow">
                 16:23:06
               </div>
-              <div className="text-[#7B879E] font-[400] text-[14px] leading-[19px] ">
+              <div className="text-[#7B879E] font-[400] text-[12px] leading-[19px] ">
                 Drawdown Reset: 02:40:00
               </div>
             </div>
           </div>
           {/********/}
           {/* Divider */}
-          <hr className=" md:min-w-full  border-[#7B879E] opacity-40" />
+          <hr className=" md:min-w-full  border-[#7B879E] opacity-20" />
           {/***********************/}
 
           {/***********************/}
           <div
             className={
-              "md:flex md:flex-col md:items-stretch md:opacity-100 md:relative md:mt-4 md:shadow-none shadow absolute top-0 left-0 right-0 z-40 overflow-y-auto overflow-x-hidden h-auto items-center flex-1 rounded " +
+              "md:flex md:flex-col justify-between md:items-stretch md:opacity-100 md:relative md:mt-4 md:shadow-none shadow absolute top-0 left-0 right-0 z-40 overflow-y-auto overflow-x-hidden h-auto items-center flex-1 rounded " +
               collapseShow
             }
           >
@@ -52,10 +102,7 @@ function Sidebar() {
             <div className="md:min-w-full md:hidden block pb-4 mb-4 border-b border-solid border-blueGray-200 text-white">
               <div className="flex flex-wrap">
                 <div className="w-6/12">
-                  <div
-                    className="md:block text-left md:pb-2  mr-0 inline-block whitespace-nowrap text-sm uppercase font-bold p-4 px-0"
-                    // to="/"
-                  >
+                  <div className="md:block text-left md:pb-2  mr-0 inline-block whitespace-nowrap text-sm uppercase font-bold p-4 px-0">
                     Dashboard
                   </div>
                 </div>
@@ -77,117 +124,45 @@ function Sidebar() {
 
             {/********/}
             {/* Navigation */}
-            <ul className="md:flex-col md:min-w-full flex flex-col list-none">
-              <li className="items-center">
-                <div
-                  className={
-                    "text-xs uppercase py-3 font-bold block "
-                    // +
-                    // (window.location.href.indexOf("/dashboard") !== -1
-                    //   ? "text-lightBlue-500 hover:text-lightBlue-600"
-                    //   : "text-blueGray-700 hover:text-blueGray-500")
-                  }
-                  // to=""
-                >
-                  <i
+            <ul className="md:flex-col md:min-w-full flex flex-col list-none ">
+              {menus.map((items, index) => {
+                return (
+                  <li className="items-center " key={index}>
+                    <div
+                      className={
+                        "text-[16px] font-[600] py-3 leading-[22px] flex gap-2.5 items-center "
+                        // +
+                        // (window.location.href.indexOf("/dashboard") !== -1
+                        //   ? "text-lightBlue-500 hover:text-lightBlue-600"
+                        //   : "text-blueGray-700 hover:text-blueGray-500")
+                      }
+                      // to=""
+                    >
+                      {/* <i
                     className={
                       "fas fa-tv mr-2 text-sm "
-                      // +
-                      // (window.location.href.indexOf("/dashboard") !== -1
-                      //   ? "opacity-75"
-                      //   : "text-blueGray-300")
+                      +
+                      (window.location.href.indexOf("/dashboard") !== -1
+                        ? "opacity-75"
+                        : "text-blueGray-300")
                     }
-                  ></i>{" "}
-                  Dashboard
-                </div>
-              </li>
-
-              <li className="items-center">
-                <div
-                  className={
-                    "text-xs uppercase py-3 font-bold block "
-                    // +
-                    // (window.location.href.indexOf("/products") !== -1
-                    //   ? "text-lightBlue-500 hover:text-lightBlue-600"
-                    //   : "text-blueGray-700 hover:text-blueGray-500")
-                  }
-                  // to=""
-                >
-                  <i
-                    className={
-                      "fa-brands fa-dropbox mr-2 text-sm "
-                      // +
-                      // (window.location.href.indexOf("/products") !== -1
-                      //   ? "opacity-75"
-                      //   : "text-blueGray-300")
-                    }
-                  ></i>{" "}
-                  Products
-                </div>
-              </li>
-
-              <li className="items-center">
-                <div
-                  className={
-                    "text-xs uppercase py-3 font-bold block "
-                    // +
-                    // (window.location.href.indexOf("/customer") !== -1
-                    //   ? "text-lightBlue-500 hover:text-lightBlue-600"
-                    //   : "text-blueGray-700 hover:text-blueGray-500")
-                  }
-                  // to=""
-                >
-                  <i
-                    className={
-                      "fa-solid fa-user mr-2 text-sm "
-                      // +
-                      // (window.location.href.indexOf("/customer") !== -1
-                      //   ? "opacity-75"
-                      //   : "text-blueGray-300")
-                    }
-                  ></i>{" "}
-                  Customers
-                </div>
-              </li>
-
-              <li className="items-center">
-                <div
-                  className={
-                    "text-xs uppercase py-3 font-bold block "
-                    // +
-                    // (window.location.href.indexOf("orders") !== -1
-                    //   ? "text-lightBlue-500 hover:text-lightBlue-600"
-                    //   : "text-blueGray-700 hover:text-blueGray-500")
-                  }
-                  // to=""
-                >
-                  <i
-                    className={
-                      "fa-regular fa-hourglass-half mr-2 text-sm "
-                      // +
-                      // (window.location.href.indexOf("/orders") !== -1
-                      //   ? "opacity-75"
-                      //   : "text-blueGray-300")
-                    }
-                  ></i>{" "}
-                  Orders
-                </div>
-              </li>
-
-              <li className="items-center">
-                <div className={"text-xs uppercase py-3 font-bold block "}>
-                  <i
-                    className={"fa-solid fa-right-from-bracket mr-2 text-sm "}
-                  ></i>{" "}
-                  logout
-                </div>
-              </li>
+                  ></i>*/}
+                      <div className="flex self-center">
+                        <img src={items.icon} alt="" srcset="" />
+                      </div>
+                      <div className="text-[#7B879E]">{items.name}</div>
+                    </div>
+                  </li>
+                );
+              })}
             </ul>
             {/********/}
-            {/* Divider */}
-            <hr className="my-4 md:min-w-full" />
-            {/********/}
-
+            <div className="flex flec-col justify-center gap-3 my-4 background_green w-full py-4 rounded-[6px]">
+              <div className="flex self-center">
+                <img src={reset} alt="" />
+              </div>
+              <div className="text-center">Reset Account</div>
+            </div>
             {/***********************/}
           </div>
         </div>
